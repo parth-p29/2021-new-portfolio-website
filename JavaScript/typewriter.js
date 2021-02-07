667976f74300d4e1a0345ca63c73b904eaca3f3b
Your Name <you@example.com>
2021-02-06 20:56:22 -0500

    commit

--- JavaScript/typewriter.js
+++ JavaScript/typewriter.js
@@ -1,71 +1,46 @@
+var app = document.querySelector('.typing');
+ 
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(800)
+  .typeString('software developer.')
+  .pauseFor(1500)
+  .deleteChars(19)
+  .pauseFor(1000)
 
-const texts = ['software developer....', 'leader....', 'designer....', 'problem-solver....'];
+  //
 
-let count = 0;
-let index = 0;
-let current_word = '';
-let letter = '';
-let rev_index = 0;
+  .typeString('problem-solver.')
+  .pauseFor(1500)
+  .deleteChars(15)
+  .pauseFor(1000)
 
+  //
 
-function type(){
+  .typeString('leader.')
+  .pauseFor(1500)
+  .deleteChars(7)
+  .pauseFor(1000)
 
+  //
 
-    current_word = texts[count];
+  .typeString('life-long learner.')
+  .pauseFor(1500)
+  .deleteChars(39)
+  .pauseFor(1000)
 
-    rev_index = current_word.length;
-    
-    letter = current_word.substring(0, index++);
+  //
 
-    document.querySelector('.typing').textContent = letter;
-
-    if (letter == current_word){
-
-        count++;
-        
-        if (count == 4){
-            count = 0;
-        }
-
-        
-        index = 0;
-    }
-
-    setTimeout(type, 350);
-
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    setTimeout(type, 300);
-
-})
-
+  .typeString('designer.')
+  .pauseFor(1500)
+  .deleteChars(9)
+  .pauseFor(1000)
 
 
+  
+  .start();
 
-
-
-
-/*
-setInterval(() => {
-    
-
-    current_text = texts[count];
-    letter = current_text.slice(0, ++index);
-    
-    document.querySelector('.typing').textContent = letter;
-    
-    if(document.querySelector('.typing').textContent == current_text){
-
-        //once word is complete, I need to add code to make it backspace the word
-
-
-        count++
-        index = 0;
-
-    }
-
-
-}, 400);
-*/
